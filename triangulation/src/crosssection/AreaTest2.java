e05293f07fceb992e5b85faaa1da227507621ba3
konstantin8105 <konstantin8105@gmail.com>
2016-05-07 16:45:56 +0300

    prepare for release

--- triangulation/src/crosssection/AreaTest2.java
+++ triangulation/src/crosssection/AreaTest2.java
@@ -1,9 +1,10 @@
 package crosssection;
 
-import triangulation.elements.Point;
 import org.junit.Assert;
 import org.junit.Test;
 import triangulation.Triangulation;
+import triangulation.elements.Point;
+import triangulation.geometry.Precisions;
 
 import java.util.Arrays;
 
@@ -57,39 +58,29 @@ public class AreaTest2 {
             };
             Triangulation triangulation = new Triangulation(Arrays.asList(points));
             System.out.println("shortDistance = " + String.format("%.2e", shortDistance) + ": Area = " + String.format("%.2e", Area.area(triangulation.getMesh())));
-            Assert.assertTrue(Area.area(triangulation.getMesh()) > 0);
+
+            if (shortDistance / Math.sqrt(2) > Precisions.epsilon()) {
+                Assert.assertTrue(Area.area(triangulation.getMesh()) > 0);
+            }
         }
     }
 
     @Test
     public void triangleSmall2() throws Exception {
-        double shortDistance = 1.0D;
+        double longDistance = 1.0D;
         for (int i = 0; i < 30; i++) {
-            shortDistance *= 10;
+            longDistance *= 10;
             Point[] points = new Point[]{
                     new Point(0.0D, 0.0D),
                     new Point(1.0D, 1.0D),
-                    new Point(shortDistance + 0.5D, 0.5D)
+                    new Point(longDistance + 0.5D, 0.5D)
             };
             Triangulation triangulation = new Triangulation(Arrays.asList(points));
-            System.out.println("shortDistance = " + String.format("%.2e", shortDistance) + ": Area = " + String.format("%.2e", Area.area(triangulation.getMesh())));
-            Assert.assertTrue(Area.area(triangulation.getMesh()) > 0);
-        }
-    }
+            System.out.println("longDistance = " + String.format("%.2e", longDistance) + ": Area = " + String.format("%.2e", Area.area(triangulation.getMesh())));
 
-    @Test
-    public void triangleSmall3() throws Exception {
-        double shortDistance = 1.0D;
-        for (int i = 0; i < 30; i++) {
-            shortDistance *= 10;
-            Point[] points = new Point[]{
-                    new Point(0.0D, 0.0D),
-                    new Point(shortDistance, shortDistance),
-                    new Point(shortDistance*0.5D+1.0D, shortDistance*0.5D)
-            };
-            Triangulation triangulation = new Triangulation(Arrays.asList(points));
-            System.out.println("shortDistance = " + String.format("%.2e", shortDistance) + ": Area = " + String.format("%.2e", Area.area(triangulation.getMesh())));
-            Assert.assertTrue(Area.area(triangulation.getMesh()) > 0);
+            if (Math.sqrt(2) / longDistance > Precisions.epsilon()) {
+                Assert.assertTrue(Area.area(triangulation.getMesh()) > 0);
+            }
         }
     }
 }
